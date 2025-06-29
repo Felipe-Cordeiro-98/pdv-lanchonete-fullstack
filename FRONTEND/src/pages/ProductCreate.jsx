101a50c05aba4fd8f1a383a6f1a6ab5f59a07a1f
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2025-06-29 20:38:22 -0300

    feat: criar página para cadastro de produtos com integração à API

---
+++ FRONTEND/src/pages/ProductCreate.jsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import ProductFormPage from "../components/ProductFormPage";
+import api from "../services/api";
+
+export default function ProductCreate() {
+    const navigate = useNavigate();
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        api.get("/categories")
+            .then((res) => setCategories(res.data))
+            .catch((err) => console.error("Erro ao carregar categorias", err));
+    }, []);
+
+    const handleSave = async (data) => {
+        try {
+            console.log(data)
+            await api.post("/products", {
+                name: data.name,
+                price: data.price,
+                stockQuantity: data.stockQuantity,
+                categoryId: data.categoryId,
+            });
+            alert("Produto cadastrado com sucesso!");
+            navigate("/products");
+        } catch (error) {
+            console.error("Erro ao cadastrar produto", error);
+            alert("Erro ao cadastrar produto.");
+        }
+    };
+
+    return (
+        <div className="container h-full">
+            <ProductFormPage title="Cadastrar Produto" categories={categories} handleForm={handleSave} />
+        </div>
+    );
+}
