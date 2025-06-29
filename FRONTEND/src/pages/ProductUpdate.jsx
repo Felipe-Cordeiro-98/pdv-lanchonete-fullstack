3fd2a0c8701861702797cb52712e24466af8a173
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2025-06-29 20:38:41 -0300

    feat: adicionar página para atualização de produtos com integração à API

---
+++ FRONTEND/src/pages/ProductUpdate.jsx
@@ -0,0 +1,47 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import ProductFormPage from "../components/ProductFormPage";
+import api from "../services/api";
+
+export default function ProductUpdate() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [product, setProduct] = useState(null);
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        api.get(`/products/${id}`).then((res) => setProduct(res.data));
+        api.get("/categories").then((res) => setCategories(res.data));
+    }, [id]);
+
+    const handleSave = async (data) => {
+        try {
+            await api.put(`/products/${id}`, {
+                name: data.name,
+                price: data.price,
+                stockQuantity: data.stockQuantity,
+                categoryId: data.categoryId,
+            });
+            alert("Produto atualizado com sucesso!");
+            navigate("/products"); // volta para listagem
+        } catch (error) {
+            console.error("Erro ao atualizar produto", error);
+            alert("Erro ao atualizar produto.");
+        }
+    };
+
+    // if (!product || categories.length === 0) return <p>Carregando...</p>;
+
+    return (
+        <div className="container h-full">
+            <ProductFormPage
+                title="Atualizar Produto"
+                product={product}
+                categories={categories}
+                handleForm={handleSave}
+                params={id}
+            />
+        </div>
+    );
+}
